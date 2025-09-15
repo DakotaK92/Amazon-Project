bad41ac0dd3a606be514aab290c36ce8d8f66686
DakotaK92 <dakotaking92@gmail.com>
2025-09-15 15:24:24 -0400

    more tests pushed to main

--- tests-jasmine/lib/jasmine-5.1.1/moneyTest.js
+++
@@ -1,8 +0,0 @@
-
-import {formatCurrency} from '../scripts/utils/money.js';
-
-describe('test suite: formatCurrency', () => {
-    it('convert cents into dollars', () => {
-        expect(formatCurrency(2095)).toEqual('20.95');
-    });
-})
