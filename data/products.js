298ea471bbee102bec0a80750d06700753e0ac56
DakotaK92 <dakotaking92@gmail.com>
2025-09-16 12:14:34 -0400

    learn more details abut class and this

--- data/products.js
+++ data/products.js
@@ -36,7 +36,6 @@ class Product {
   }
 
   extraInfoHTML() {
-    //super.extraInfoHTML();
     return '';
   }
 }
@@ -55,6 +54,38 @@ class Clothing extends Product{
     `;
   }
 }
+
+/*
+const date = new Date();
+console.log(date);
+date.toLocaleTimeString();
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+console.log(this);
+
+const object2 = {
+  a: 2,
+  b: this.a
+};
+*/
+
+/*
+function logThis() {
+  console.log(this);
+};
+logThis();
+logThis.call('hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method();
+*/
 
 export const products = [
   {
